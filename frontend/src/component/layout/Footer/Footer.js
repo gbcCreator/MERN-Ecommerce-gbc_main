74a819df7b39fbb8572dfae4cd95c75f171f5730
Dev kumar <gbc.creator.code@gmail.com>
2024-07-29 23:28:10 +0530

    change footer

--- frontend/src/component/layout/Footer/Footer.js
+++ frontend/src/component/layout/Footer/Footer.js
@@ -19,7 +19,7 @@ const Footer = () => {
       </div>
 
       <div className="midFooter">
-        <h1>GBC-ECOMMERCE.</h1>
+        <h1>GBC-ECOMMERCE</h1>
         <p>High Quality is our first priority</p>
 
         <p>Copyrights {currentyear} &copy; Dev-kumar</p>
